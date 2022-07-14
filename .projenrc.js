59add130d988456346a1853679d12496fedbb991
Sam Sussman <sussmansa@gmail.com>
2022-07-14 16:32:03 -0500

    first sample project

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,37 @@
 const { LernaProject } = require("lerna-projen");
+const {FunctionlessProject} = require("@functionless/projen");
+
 const project = new LernaProject({
   defaultReleaseBranch: "main",
-  devDeps: ["lerna-projen"],
+  devDeps: ["lerna-projen", '@functionless/projen'],
   name: "functionless-samples",
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+
+const functionless1 = new FunctionlessProject({
+  cdkVersion: '2.31.1',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@functionless/projen', "ts-node@latest"],
+  parent: project,
+  outdir: "packages/sample1",
+  name: 'sample1',
+  tsconfig: {
+    compilerOptions: {
+      skipLibCheck: true
+    }
+  },
+  eslintOptions: {
+    lintProjenRc: true,
+  },
+  prettier: {}
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addSubProject(functionless1);
+
 project.synth();
