273051c49e25cf7b30bab232e4bc92860f836539
Sam Sussman <sussmansa@gmail.com>
2022-07-22 15:08:43 -0500

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,9 @@
-const { LernaProject } = require("lerna-projen");
-const project = new LernaProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["lerna-projen"],
-  name: "functionless-samples",
+import { TurborepoProject } from 'projen-turborepo';
+const project = new TurborepoProject({
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-turborepo'],
+  name: 'functionless-samples',
+  projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
