77d1d7009e3c2deb6e0623bf2c98841838c4e63a
Sam Sussman <sussmansa@gmail.com>
2022-07-23 09:49:55 -0500

    feat: turbo (#1)

---
+++ .projenrc.ts
@@ -0,0 +1,33 @@
+import { TurborepoProject } from 'projen-turborepo';
+import {FunctionlessProject} from'@functionless/projen';
+
+const turbo = new TurborepoProject({
+  defaultReleaseBranch: 'main',
+  devDeps: ['projen-turborepo', '@functionless/projen'],
+  name: 'functionless-samples',
+  projenrcTs: true,
+  turbo: {
+    pipeline: {
+      "deploy" : {},
+      "synth": {},
+      "prepare": {}
+    }
+  }
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+
+// prepare is not run when install sub-package, run prepare to run the functionless requirement `ts-patch install -s`
+turbo.setScript("postinstall", "turbo run prepare")
+
+new FunctionlessProject({
+  parent: turbo,
+  defaultReleaseBranch: 'main',
+  name: 'sample1',
+  outdir: 'packages/sample1',
+  cdkVersion: '2.33.0'
+})
+
+turbo.synth();
