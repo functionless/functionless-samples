d40dc8ec79ef79cba8726e288dde67db3b28d68f
Sam Sussman <sussmansa@gmail.com>
2022-07-23 12:51:16 -0500

    chore: eslint and prettier (#2)
    * feat: eslint and prettier
    
    * projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,33 +1,62 @@
-import { TurborepoProject } from 'projen-turborepo';
-import {FunctionlessProject} from'@functionless/projen';
+import { FunctionlessProject } from "@functionless/projen";
+import { Husky } from "@mountainpass/cool-bits-for-projen";
+import { TurborepoProject } from "projen-turborepo";
 
 const turbo = new TurborepoProject({
-  defaultReleaseBranch: 'main',
-  devDeps: ['projen-turborepo', '@functionless/projen'],
-  name: 'functionless-samples',
+  defaultReleaseBranch: "main",
+  devDeps: [
+    "projen-turborepo",
+    "@functionless/projen",
+    "lint-staged",
+    "@mountainpass/cool-bits-for-projen",
+  ],
+  name: "functionless-samples",
   projenrcTs: true,
   turbo: {
     pipeline: {
-      "deploy" : {},
-      "synth": {},
-      "prepare": {}
-    }
-  }
-
+      deploy: {},
+      synth: {},
+      prepare: {},
+    },
+  },
+  prettier: true,
+  eslintOptions: {
+    dirs: [],
+    lintProjenRc: true,
+    prettier: true,
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
 
 // prepare is not run when install sub-package, run prepare to run the functionless requirement `ts-patch install -s`
-turbo.setScript("postinstall", "turbo run prepare")
+turbo.setScript("postinstall", "turbo run prepare");
 
 new FunctionlessProject({
   parent: turbo,
-  defaultReleaseBranch: 'main',
-  name: 'sample1',
-  outdir: 'packages/sample1',
-  cdkVersion: '2.33.0'
-})
+  defaultReleaseBranch: "main",
+  name: "sample1",
+  outdir: "packages/sample1",
+  cdkVersion: "2.33.0",
+});
+
+const packageJson = turbo.tryFindObjectFile("package.json");
+
+packageJson?.addOverride("lint-staged", {
+  "*.{tsx,jsx,ts,js,json,md}": ["turbo run eslint -- --"],
+});
+
+new Husky(turbo, {
+  huskyHooks: {
+    "pre-commit": ["#!/bin/sh", "npx -y lint-staged"],
+  },
+});
+
+const eslintJson = turbo.tryFindObjectFile(".eslintrc.json");
+eslintJson?.addOverride("parserOptions.project", [
+  "./tsconfig.dev.json",
+  "./packages/sample1/tsconfig.dev.json",
+]);
 
 turbo.synth();
